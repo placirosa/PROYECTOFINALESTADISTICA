c33ab2a5cd8bcb49f41f32a7b6a9df519d1a3307
Placido <placidokds788@gmail.com>
2026-06-23 00:04:02 -0400

    version1

--- media_geometrica.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from scipy.stats import gmean
-df_est = pd.read_csv("datos_estudiantes.csv")
-
-media_geom = gmean(df_est['edad'])
-
-print("--- MEDIA GEOMÉTRICA ---")
-print(f"La media geométrica de la edad es: {media_geom:.2f}")
